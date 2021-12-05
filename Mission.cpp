48b5f70f8229058223259234b92c5edc195b09cc
kas054 <80067024+kas054@users.noreply.github.com>
2021-12-05 21:49:05 +0300

    Update Mission.cpp

--- Mission.cpp
+++ Mission.cpp
@@ -6,12 +6,12 @@
 using namespace Basic;
 namespace Menu{
 
-    Basic::Coordinate Mission::get_coord_A_B(int i) const {
+    Basic::Coordinate *Mission::get_coord_A_B(int i) const {
         Basic::Coordinate answer;
         if (i == 0) answer =  coordinates_A;
         else if (i == 1) answer = coordinates_B;
         else throw "No such base";
-        return answer;
+        return &answer;
     }
 
     void Mission::set_coord_A_B(int i, int x, int y){
@@ -82,8 +82,8 @@ namespace Menu{
         return cur_ship;
     }
 
-    void Mission::buy_ship(std::string ship_type){
-        std::map <std::string, Ships::Ship> :: iterator it;
+    void Mission::buy_ship(int c_p, std::string ship_type) {
+        std::map<std::string, Ships::Ship>::iterator it;
         Ships::Ship *new_ship;
         int cost;
         it = (config->ship).find(ship_type);
@@ -94,15 +94,31 @@ namespace Menu{
         *new_ship = it->second;
         cost = new_ship->get_property(4);
 
-        //max_money - spend_money > 0 && max_count_ship_c > current_size of convoy
-        if ((this->get_properties(0) - this->get_properties(1)) > 0 && (this->get_properties(6) > convoy->get_count())) {
-            convoy->add_ship(new_ship, get_coord_A_B(0)); // помещаем корабль на базу А
-            this->set_properties(1, cost + this->get_properties(1)); // change spend_money
+        switch (c_p) {
+            case 0: { // add ship to convoy
+                //max_money - spend_money > 0 && max_count_ship_c > current_size of convoy
+                if ((this->get_properties(0) - this->get_properties(1)) > 0 &&
+                    (this->get_properties(6) > convoy->get_count())) {
+                    convoy->add_ship(new_ship, *get_coord_A_B(0)); // помещаем корабль на базу А
+                    this->set_properties(1, cost + this->get_properties(1)); // change spend_money
+                } else {
+                    delete new_ship;
+                    throw "You do not have money or free place in convoy";
+                }
+            }
+            case 1:{ // add pirates
+                // max_count_ship_p > current_size of pirates
+                if ((this->get_properties(7) > pirates->get_count()))
+                    convoy->add_ship(new_ship, *get_coord_A_B(1)); // помещаем корабль на базу B
+                else{
+                    delete new_ship;
+                    throw "Too many pirates";
+                }
+            }
         }
-        else throw "You do not have money or free place in convoy";
     }
 
-    void Mission::sell_ship(int c_p, std::string name){
+    void Mission::sell_ship(std::string name){
         Table <std::string, Info> :: Iterator it;
         int cost;
         it = convoy->find(name);
@@ -117,19 +133,152 @@ namespace Menu{
     void Mission::del_ship(int c_p, std::string name){ // remove from Basic_config
         Table <std::string, Info> :: Iterator it;
         std::string type;
+        std::map<std::string, Ships::Ship> :: iterator map_it;
+
         switch (c_p){
-            case 0:
+            case 0: {
                 it = convoy->find(name);
                 if (it == convoy->end()) throw "No such ship";
-            case 1:
+            }
+            case 1: {
                 it = pirates->find(name);
                 if (it == pirates->end()) throw "No such ship";
+            }
         }
         type = (*it).info.ship->get_type();
-        
-        std::map<std::string, Ships::Ship> :: iterator map_it;
-        
 
+        map_it = (config->ship).find(type);
+
+        if (map_it == (config->ship).end()) throw "No such ship";
+
+        sell_ship( name);
+        config->ship.erase(map_it);
+    }
+
+    void Mission::add_cargo(std::string name, int weight){
+        Table <std::string, Info> :: Iterator it;
+        Ships::Transport_ship *cur_ship;
+
+        it = convoy->find(name);
+        if (it == convoy->end()) throw "No such ship";
+
+        cur_ship = dynamic_cast<Ships::Transport_ship *>((*it).info.ship);
+
+        // max_cargo >= cur_cargo + weight
+        if (cur_ship->get_info_cargo(0) >= cur_ship->get_info_cargo(1) + weight) {
+            cur_ship->set_cur_cargo(weight);
+            this->properties[4] += weight; // cur_cargo += weight
+        }
+    }
+
+    void Mission::buy_armament(int c_p, std::string name, int place, std::string armament){
+        Table<std::string, Info> :: Iterator it;
+        std::vector <Basic::Armament> :: const_iterator vec_it;
+        Ships::Security_ship *cur_ship;
+        Basic::Armament *new_armament = nullptr;
+        int cost;
+
+        switch(c_p){
+            case 0: {
+                it = convoy->find(name);
+                if (it == convoy->end()) throw "No such ship";
+            }
+            case 1: {
+                it = pirates->find(name);
+                if (it == pirates->end()) throw "No such ship";
+            }
+        }
+
+        cur_ship = dynamic_cast<Ships::Security_ship *>((*it).info.ship);
+
+        for (vec_it = (config->armament).cbegin(); vec_it != config->armament.cend(); ++vec_it){
+            if (vec_it->get_type() == armament) {
+                new_armament = new Basic::Armament;
+                *new_armament = *vec_it;
+                break;
+            }
+        }
+
+        cost = vec_it->get_property(5);
+        //max_money - spend_money > 0
+        if ((c_p == 0 && ((this->get_properties(0) - this->get_properties(1)) > 0)) || c_p == 1) {
+            try {
+                cur_ship->add_armament(new_armament, place);
+                if (c_p == 0)
+                    this->set_properties(1, cost + this->get_properties(1)); // change spend_money
+            }
+            catch (const char *e) { // покупка невозможна
+                std::cout << *e << std::endl;
+                if (c_p == 0)
+                    this->set_properties(1, this->get_properties(1) - cost); // change spend_money
+                delete new_armament;
+            }
+        }
+
+    }
+
+    void Mission::change_armament(int c_p, std::string name, int place, int property, int new_value, std::string type){
+        Table<std::string, Info> :: Iterator it;
+        std::vector <Basic::Armament> :: iterator vec_it;
+        Ships::Security_ship *cur_ship;
+
+        switch(c_p){
+            case 0: {
+                it = convoy->find(name);
+                if (it == convoy->end()) throw "No such ship";
+            }
+            case 1: {
+                it = pirates->find(name);
+                if (it == pirates->end()) throw "No such ship";
+            }
+        }
+
+        cur_ship = dynamic_cast<Ships::Security_ship *>((*it).info.ship);
+        try {
+            cur_ship->change_armament(place, property, new_value, type);
+        }
+        catch (const char *e) {
+            std::cout << *e << std::endl;
+        }
+    }
+
+    void Mission::sell_armament(int c_p, std::string name, int place){
+        Table<std::string, Info> :: Iterator it;
+        Ships::Security_ship *cur_ship;
+        Basic::Armament *cur_armament;
+        int cost;
+
+        switch(c_p){
+            case 0: {
+                it = convoy->find(name);
+                if (it == convoy->end()) throw "No such ship";
+            }
+            case 1: {
+                it = pirates->find(name);
+                if (it == pirates->end()) throw "No such ship";
+            }
+        }
+        cur_ship = dynamic_cast<Ships::Security_ship *>((*it).info.ship);
+
+        try {
+            cur_armament = cur_ship->get_info_armament(place);
+        }
+        catch (const char *e) {
+            std::cout << *e << std::endl;
+        }
+
+        cost = cur_armament->get_property(5);
+
+        try {
+            cur_ship->remove_armament(place);
+            if (c_p == 0)
+                this->set_properties(1, this->get_properties(1) - cost); // change spend_money
+        }
+        catch (const char *e) { // удаление невозможно
+            std::cout << *e << std::endl;
+            if (c_p == 0)
+                this->set_properties(1, this->get_properties(1) + cost); // change spend_money
+        }
     }
 
 }
